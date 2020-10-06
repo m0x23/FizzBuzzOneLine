824852d9756db7ac02cbe189ba57a46d2e6c3ba1
Mark Breukelman <mark.breukelman@outlook.com>
2020-10-06 21:00:53 +0200

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,6 +2,6 @@ public class FizzBuzz
 {
 	public static void main(String[] args)
 	{
-		for(int i = 1; i <= 100; i++)	System.out.println(i % 3 == 0 ? i % 5 == 0 ? "FizzBuzz" : "Fizz" : i % 5 == 0 ? "Buzz" : String.valueOf(i));
+		for(int i = 1; i <= 100; i++) System.out.println(i % 3 == 0 ? i % 5 == 0 ? "FizzBuzz" : "Fizz" : i % 5 == 0 ? "Buzz" : String.valueOf(i));
 	}
 }
